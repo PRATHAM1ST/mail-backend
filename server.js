367d9af530dddfc4b2d29b444b1a57f0d0b7a799
Falgun <77203052+fxlgun@users.noreply.github.com>
2022-12-08 12:09:16 +0530

    added mail

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { get } = require("http");
+const app = express();
+const nodemailer = require("nodemailer");
+
+app.use(express.json())
+
+transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    //host: 'myhost',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'falgunagohel@gmail.com',
+      pass: 'joulpshllydsuony'
+    }
+  });
+
+  app.post("/sendmail", (req, res) => {
+    const mailData = req.body;
+    console.log(mailData);
+    transporter.sendMail(mailData, async function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+  })
+
+
+  app.listen(1000);
+  
